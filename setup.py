ff7f83c3d51df7a8970d95189bc7a16251c6faac
Michael Howitz <mh@gocept.com>
2009-02-21 14:04:24 +0000

    Back to development: 1.2.2 dev

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.2.1'
+version = '1.2.2 dev'
 
 setup(
     name='z3c.layer.pagelet',
