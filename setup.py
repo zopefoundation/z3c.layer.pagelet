a2ae65057805c68339e94508de3f7b0d20ccab4f
Michael Howitz <mh@gocept.com>
2012-02-23 19:55:15 +0000

    Back to development: 1.10.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
 
 setup(
     name='z3c.layer.pagelet',
-    version='1.10.0',
+    version='1.10.1dev',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description = "Pagelet layer setup for Zope 3",
