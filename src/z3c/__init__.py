00167e2c65cb10ba425ba16792a485edcac74b21
Adam Groszer <agroszer@gmail.com>
2015-11-04 15:52:00 +0100

    Standardize namespace __init__, pylint burps otherwise

--- src/z3c/__init__.py
+++ src/z3c/__init__.py
@@ -1,2 +1,7 @@
 # this is a namespace package
-__import__('pkg_resources').declare_namespace(__name__)
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
