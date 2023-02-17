12a280f8ec1f7e0d381fc68677abdd6b3edfd050
Michael Howitz <mh@gocept.com>
2023-02-17 08:31:11 +0100

    Preparing release 3.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@
 CHANGES
 =======
 
-3.0 (unreleased)
+3.0 (2023-02-17)
 ----------------
 
 - Add support for Python 3.8, 3.9, 3.10, 3.11.
--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ TESTS_REQUIRE = [
 
 setup(
     name='z3c.layer.pagelet',
-    version='3.0.dev0',
+    version='3.0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.dev',
     description="Pagelet layer setup for Zope 3",
